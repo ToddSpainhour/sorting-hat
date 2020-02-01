24f59d85eca8b9203c1519c33e741fb8082517e7
Todd Spainhour <toddspainhour@gmail.com>
2020-02-01 12:37:44 -0600

    print to dom fuctionality working

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
 console.log("Your JavaScript is connected!")
 
 
-//print to DOM function
-const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = textToPrint;
-};
+const students = [
+    {
+        name: "Todd Spainhour",
+        house: "Magic House",
+
+    },
+
+    {
+        name: "Jimmy Bobby",
+        house: "Abracabra House"
+    },
+
+    {
+        name: "Susan McWitch",
+        house: "Bright House"
+    }
+]
 
 
 const makeFormAppear = () => {
     const visibleForm = document.getElementById('letsGetSortingForm')
     letsGetSortingForm.classList.remove('invisible')
     letsGetSortingForm.classList.add('visible')
-    console.log("you eventlistener is working")
+    console.log("you Let's Get Sorting eventlistener is working")
 }
 
+
+//print to DOM functionality
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+
+const buildFormCard = () => {
+    let domString = '';
+    for (i = 0; i < students.length; i++) {
+
+        domString += `<div class="printCardsHere" style="width: 18rem;">`
+            domString += `<div id=".bg-secondary">` //make sure this is correct
+                domString +=  `<div class="card-body">`
+                    domString += `<h5 class="card-title">${students[i].name}</h5>`
+                    domString += `<p class="card-text">Randomly Selected House</p>`
+                    domString +=`<a href="#" class="btn btn-primary">Expell</a>`
+                domString += `</div>`  
+            domString += `</div>`
+        domString += `</div>`
+
+
+printToDom('printCardsHere', domString)
+
+}
+}
 
 
 
@@ -26,20 +65,7 @@ const events = () => {
 
 const init = () => {
 events()
+buildFormCard()
 }
 
 init()
-
-//add print to dom stuff to code below
-/*
-const buildFormCard = () => {
-    let domString = '';
-            domString +=   ;
-
-printToDom('sendFormHere', domString)
-
-}
-
-//buildFormCard(events)
-
-*/
