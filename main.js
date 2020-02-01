fdeed9396fb1390c66a974ae3d80b274aa991d71
Todd Spainhour <toddspainhour@gmail.com>
2020-02-01 10:52:14 -0600

    get sorting button makes form appear

--- main.js
+++ main.js
@@ -8,41 +8,38 @@ const printToDom = (divId, textToPrint) => {
 };
 
 
-
-//Make stuff appear
-const events = () => {
-    document.getElementById('clickMeToMakeFormAppear').addEventListener('click', buildFormCard)
+const makeFormAppear = () => {
+    const visibleForm = document.getElementById('letsGetSortingForm')
+    letsGetSortingForm.classList.remove('invisible')
+    letsGetSortingForm.classList.add('visible')
+    console.log("you eventlistener is working")
 }
 
 
-//add print to dom stuff to code below
 
+
+//Click event that make stuff appear
+const events = () => {
+    document.getElementById('clickMeToMakeFormAppear').addEventListener('click', makeFormAppear)
+}
+
+
+const init = () => {
+events()
+}
+
+init()
+
+//add print to dom stuff to code below
+/*
 const buildFormCard = () => {
     let domString = '';
-            domString +=   `<form class="form-inline">
-            <label class="sr-only" for="inlineFormInputName2">Name</label>
-            <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe">
-        
-            <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-            <div class="input-group mb-2 mr-sm-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text">@</div>
-            </div>
-            <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
-            </div>
-        
-            <div class="form-check mb-2 mr-sm-2">
-            <input class="form-check-input" type="checkbox" id="inlineFormCheck">
-            <label class="form-check-label" for="inlineFormCheck">
-                Remember me
-            </label>
-            </div>
-        
-            <button type="submit" class="btn btn-primary mb-2">Submit</button>
-        </form>`;
+            domString +=   ;
 
 printToDom('sendFormHere', domString)
 
 }
 
-buildProjectsCards(events)
+//buildFormCard(events)
+
+*/
