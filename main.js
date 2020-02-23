86d35d21d6742a1ad9c7c83b0ebcb8aad9d0c1b9
Todd Spainhour <toddspainhour@gmail.com>
2020-02-22 23:32:58 -0600

    expell button functional

--- main.js
+++ main.js
@@ -1,55 +1,52 @@
 
-//todo list
-//create a way to assign a unique ID to each card
-//expell button
-//bootatrap
-const students = [
-    
-]
-
+const students = []
 
 const studentHouses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
-
-//let randomHouse = studentHouses[Math.floor(Math.random()*studentHouses.length)];
-
 let randomHouse = () => {
     return studentHouses[Math.floor(Math.random()*studentHouses.length)]
-}
-
-
-
-
+};
 
 //this function is used for the click event to make the form appear
 const makeFormAppear = () => {
     letsGetSortingForm.classList.remove('invisible')
     letsGetSortingForm.classList.add('visible')
-}
-
-
+};
 
 //take info from input field and push it to the students array
-
-const studentNameSubmission = () =>{
+const studentNameSubmission = (e) =>{
     
     let studentNameEnteredByUser = document.getElementById('inputAreaForUsertoEnterName').value;
     students.push({name: studentNameEnteredByUser, house: randomHouse(), IdNumber: uniqueIdNumber()}); //I wasn't including the object brackets {} before... also adding the () after the newly created randomHouse function got it working
-    buildFormCard()
-    //init() //should this be here?
-    
-}
+    buildFormCard(students);
+    addExpelClickEvent(); 
+    e.preventDefault();
 
+};
+
+// function that loops through all objects and adds click event to expel button on each object card
+const addExpelClickEvent = () => {
+
+    for (let i = 0; i < students.length; i++) {
+        let expelButton = document.getElementsByClassName('expelBtn');
+        expelButton[i].addEventListener('click', expel);
+    }
+};
+
+// expel click event function that removes selected student and reprints the array
+const expel = (e) => {
+    for (let i = 0; i < students.length; i++) {
+        if (e.target.id == students[i].IdNumber) {
+            students.splice(i, 1);
+            buildFormCard(students);
+        }
+    }
+};
 
 //creates unique ID number to add to each card
 let uniqueIdNumber = () => {
     return Date.now();
 }
-
-
-
-
-
 
 //print to DOM functionality
 const printToDom = (divId, textToPrint) => {
@@ -57,76 +54,31 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 };
 
-
-
+//builds student card
 const buildFormCard = () => {
     let domString = '';
     for (i = 0; i < students.length; i++) {
-
-    
-        
             domString +=  `<div class="card-body col-sm-3 bg-light rounded-lg m-1">`
                     domString += `<h5 class="card-title text-center">${`Name: `}${students[i].name}</h5>`
                     domString += `<p class="card-text text-center">${`House: `}${students[i].house}</p>`
-                    domString += `<p class="card-text text-center">${`Unique ID: `}${students[i].IdNumber}</p>`
-                    domString +=`<button id="${students[i].IdNumber}" class="expell btn btn-primary">Expell</button>`
+                    domString += `<p class="card-text text-center">${`IdNumber: `}${students[i].IdNumber}</p>`
+                    domString +=`<button id="${students[i].IdNumber}" class="expelBtn btn btn-primary">Expel</button>`
             domString += `</div>`  
-            
-        
-    
-//`<button id="${students[i].IdNumber}" type="button" class="expel-button btn btn-primary">Expel</button>`
-
-
-
-printToDom('printCardsHere', domString)
-
-}
-}
-
-
-const expellButton = document.getElementById('${students[i].IdNumber}');
-
-
-
-//i need to create a event listener for the expell button that summons a function that uses splice to get rid of only that card. Then reprint.
-const expellStudent = () => {
-    
-    if(e.target.id === students[i].IdNumber){
-        students.splice(i, 1)
-        return console.log("This is your expell button")
-    }
-    ;
-    buildFormCard() 
-    //init()
+printToDom('printCardsHere', domString);
+};
 };
 
-
-
-
-
-const madeUpThing = () => {
-    console.log("if you see this you expell button event listener is working.")
-}
-
-
-
-
-
-//Click events that make stuff appear
+//Click events that make other elements appear
 const events = () => {
     document.getElementById('clickMeToMakeFormAppear').addEventListener('click', makeFormAppear);
     document.getElementById('submitButtonForUserEnteredName').addEventListener('click', studentNameSubmission); //put a id on it not a class
-    document.getElementsById(`this.IdNumber`).addEventListener('click', madeUpThing);
-}
-
-
-
-
+    // document.getElementsByClassName(`expelBtn`).addEventListener('click', addExpelClickEvent )
+};
 
 const init = () => {
 events()
-buildFormCard()   //you need to fire this off when the user submits name
-}
+buildFormCard() 
+};
 
-init()
+init();
 
